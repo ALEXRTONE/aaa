5b9187dffa1ec4dfc44d7d2064e64bc03eff9819
Luis Rszm <luisrszm@MacBook-Air-de-Luis.local>
2025-11-18 23:41:31 -0600

    srv edit

--- idex/client/vite.config.ts
+++ idex/client/vite.config.ts
@@ -9,25 +9,25 @@ dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), flowbiteReact(), tailwindcss()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false
-      },
-      '/signup-api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false
-      },
-    },
-  }
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: process.env.VITE_API_URL,
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //     '/auth': {
+  //       target: process.env.VITE_API_URL,
+  //       changeOrigin: true,
+  //       secure: false
+  //     },
+  //     '/signup-api': {
+  //       target: process.env.VITE_API_URL,
+  //       changeOrigin: true,
+  //       secure: false
+  //     },
+  //   },
+  // }
 })
