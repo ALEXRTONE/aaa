970d683bd73b1db3c19446bdc293f6526fbcd0b8
Luis Rszm <luisrszm@MacBook-Air-de-Luis.local>
2025-11-20 01:04:29 -0600

    edit server statics

--- idex/server/src/server.ts
+++ idex/server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors'
+import path from 'path'
 // Import the connection object
 import sequelize from './config/connection.js';
 import routes from './routes/index.js'
@@ -6,12 +8,18 @@ import routes from './routes/index.js'
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../../client/dist'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
+app.use(cors({
+  origin: 'https://idex-backend-8rw0.onrender.com',
+}));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join('https://idex-backend-8rw0.onrender.com', '../../client/dist/index.html'));
+});
 
 // Connect to the database before starting the Express.js server
 sequelize.sync().then(() => {
