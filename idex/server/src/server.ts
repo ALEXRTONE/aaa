5d6944f7fe2631942473fe8492cc50a0155f0dc8
Luis Rszm <luisrszm@MacBook-Air-de-Luis.local>
2025-11-20 20:55:15 -0600

    resolved server error

--- idex/server/src/server.ts
+++ idex/server/src/server.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import cors from 'cors'
-import path from 'path'
-import { fileURLToPath } from 'url';
+// import cors from 'cors'
 // Import the connection object
 import sequelize from './config/connection.js';
 import routes from './routes/index.js'
@@ -9,20 +7,17 @@ import routes from './routes/index.js'
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static('../../client/dist'));
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.use(cors({
-  origin: 'https://idex-backend-8rw0.onrender.com',
-}));
+// app.use(cors({
+//   origin: 'https://idex-backend-8rw0.onrender.com',
+// }));
 
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-});
 
 // Connect to the database before starting the Express.js server
 sequelize.sync().then(() => {
