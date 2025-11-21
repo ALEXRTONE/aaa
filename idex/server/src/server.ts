1c88c9f2f5e401903f7769a222d5e1099deb6cfc
Luis Rszm <luisrszm@MacBook-Air-de-Luis.local>
2025-11-20 20:27:44 -0600

    resolved path error

--- idex/server/src/server.ts
+++ idex/server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url';
 // Import the connection object
 import sequelize from './config/connection.js';
 import routes from './routes/index.js'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
 app.use(express.json());
@@ -18,7 +21,7 @@ app.use(cors({
 }));
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join('https://idex-backend-8rw0.onrender.com', '../../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 // Connect to the database before starting the Express.js server
